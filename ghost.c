70cf3993359a13e2dc7633364ef43fda911ccf53
LengBTan <letng27@gmail.com>
2023-11-28 17:08:41 -0500

    Update ghost.c

--- ghost.c
+++ ghost.c
@@ -1,1 +1,9 @@
 #include "defs.h"
+
+void initGhost(GhostType *ghost, RoomType *startingRoom){
+    ghost->class = randomGhost();//randomly select a ghost type upon initialization
+    ghost->currRoom = startingRoom;
+    ghost->boredom = 0;
+}
+
+//random ghost actions
