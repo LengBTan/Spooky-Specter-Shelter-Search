92a7a22d96c0c34fd8225e45ee4c941d4d3e15e5
LengBTan <letng27@gmail.com>
2023-11-28 17:08:24 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,10 +1,7 @@
 #include "defs.h"
-//asdf
-// hello my name is kina
-int main()
-{
-    // Initialize the random number generator
-    srand(time(NULL));
+
+int main(){
+    srand(time(NULL));// Initialize the random number generator
 
     // Create the house: You may change this, but it's here for demonstration purposes
     // Note: This code will not compile until you have implemented the house functions and structures
