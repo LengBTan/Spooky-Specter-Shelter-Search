89e1278c46c53f058f945789e7801152e184c1e3
LengBTan <letng27@gmail.com>
2023-12-02 22:46:45 -0500

    Update defs.h

--- defs.h
+++ defs.h
@@ -37,33 +37,31 @@ typedef struct HunterList HunterListType;
 typedef struct EvidenceList EvidenceListType;
 typedef struct EvidenceNode EvidenceNodeType;
 
-//structs
-struct EvidenceList{//Evidence collection
-    int size;//stores the size of the EvidenceList
-    sem_t mutex;//mutex for the collection
-    EvidenceNodeType *head;//pointer to the head of the linked list
-    EvidenceNodeType *tail;//pointer to the tail
+// structs
+struct EvidenceList{ // Evidence collection
+    int size; // stores the size of the EvidenceList
+    sem_t mutex; // mutex for the collection
+    EvidenceNodeType *head; // pointer to the head of the linked list
+    EvidenceNodeType *tail; // pointer to the tail
 };
 
 struct EvidenceNode{
-    EvidenceType evidence;//data that the node is storing
-    struct EvidenceNode *next;//point to the next node
-    struct EvidenceNode *prev;//point to the previous node
+    EvidenceType evidence; // data that the node is storing
+    struct EvidenceNode *next; // point to the next node
 };
 
 struct Hunter{
-    RoomType *currRoom;//pointer to the current room the hunter is in
-    EvidenceType evType;//type of evidence that the hunter is able to scan
-    char name[MAX_STR];//name of the hunter
-    EvidenceListType *evList;//pointer to the shared evidence collection
-    int fear;//fear counter
-    int boredom;//boredom counter
+    RoomType *currRoom; // pointer to the current room the hunter is in
+    EvidenceType evType; // type of evidence that the hunter is able to scan
+    char name[MAX_STR]; // name of the hunter
+    EvidenceListType *evList; // pointer to the shared evidence collection
+    int fear; // fear counter
+    int boredom; // boredom counter
 };
 
 struct HunterNode{
     HunterType *data;
     struct HunterNode *next;
-    struct HunterNode *prev;
 };
 
 struct HunterList{
@@ -72,37 +70,36 @@ struct HunterList{
     HunterNodeType *tail;
 };
 
-struct RoomNode{//roomNodes for the RoomList
-    RoomType *data;//points to the room
-    RoomNodeType *next;//points to the next node
-    RoomNodeType *prev;//points to the previous node
+struct RoomNode{ // roomNodes for the RoomList
+    RoomType *data; // points to the room
+    RoomNodeType *next; // points to the next node
 };
 
-struct RoomList{//doubly linked list, store the head and tail
-    int size;//stores the size of the room list
+struct RoomList{ // doubly linked list, store the head and tail
+    int size; // stores the size of the room list
     RoomNodeType *head;
     RoomNodeType *tail;
 };
 
 struct Room{
-    char name[MAX_STR];//name of the room
-    RoomListType adjRooms;//linked list of adjacent rooms it is connected to
-    EvidenceListType evList;//linked list of evidence that ghost has left
-    HunterListType hunterList;//collection of hunters in the room
-    GhostType *ghost;//pointer to the ghost that is in the room
-    sem_t mutex;//semaphore to prevent the room from being modified by multiple threads
+    char name[MAX_STR]; // name of the room
+    RoomListType adjRooms; // linked list of adjacent rooms it is connected to
+    EvidenceListType evList; // linked list of evidence that ghost has left
+    HunterListType hunterList; // collection of hunters in the room
+    GhostType *ghost; // pointer to the ghost that is in the room
+    sem_t mutex; // semaphore to prevent the room from being modified by multiple threads
 };
 
 struct House{
-    HunterListType hunterList;//collection of hunters, maybe just an array of hunters?
-    RoomListType rooms;//linkedList of rooms
-    EvidenceListType evList;//shared evidence list that hunters access
+    HunterListType hunterList; // collection of hunters, maybe just an array of hunters?
+    RoomListType rooms; // linkedList of rooms
+    EvidenceListType evList; // shared evidence list that hunters access
 };
 
 struct Ghost{
-    GhostClass class;//the class of the ghost
-    RoomType *currRoom;//pointer to the current room the ghost is in
-    int boredom;//boredom counter, initialize it to 0
+    GhostClass class; // the class of the ghost
+    RoomType *currRoom; // pointer to the current room the ghost is in
+    int boredom; // boredom counter, initialize it to 0
 };
 
 
@@ -111,47 +108,47 @@ struct Ghost{
 //Room functions
 RoomType* createRoom(char *name);
 void initRoomList(RoomListType *list);
-void addRoom(RoomListType *list, RoomType *room);//add room to the room list
-void connectRooms(RoomType *room1, RoomType *room2);//connect the 2nd room to the first room
+void addRoom(RoomListType *list, RoomType *room); // add room to the room list
+void connectRooms(RoomType *room1, RoomType *room2); // connect the 2nd room to the first room
 RoomType* chooseStartingRoom(RoomListType *list, GhostType *ghost);
 
-void cleanupRoomData(RoomListType *list);//cleanup the room data
-void cleanupRoomAdj(RoomType *room);//free the adjacent rooms of room
-void cleanupRoomList(RoomListType *list);//used before the program quits
+void cleanupRoomData(RoomListType *list); // cleanup the room data
+void cleanupRoomList(RoomListType *list); // used before the program quits
 
+// for debugging, remove later?
 void printRoomAdj(RoomType *room);
 void printRoomEv(RoomType *room);
 void printRoomHunter(RoomType *room);
 void printRoom(RoomType *room);
 void printRoomList(RoomListType *list);
 
-//House functions
+// House functions
 void initHouse(HouseType *house);
-void cleanupHouse(HouseType *house);//cleanup the house before the program ends
-void populateRooms(HouseType *house);//populate the rooms in the house
+void cleanupHouse(HouseType *house); // cleanup the house before the program ends
+void populateRooms(HouseType *house); // populate the rooms in the house
 
-//Ghost functions
+// Ghost functions
 void initGhost(GhostType *ghost, RoomType *startingRoom);
-char checkHunter(GhostType *ghost);//helper function that checks if a hunter is in the room
-void* ghostAction(void *arg);//helper function to choose a random action for the ghost to take
-void ghostMove(GhostType *ghost);//ghost action to move to another room
+char checkHunter(GhostType *ghost); // helper function that checks if a hunter is in the room
+void* ghostAction(void *arg); // helper function to choose a random action for the ghost to take
+void ghostMove(GhostType *ghost); // ghost action to move to another room
 
-//Hunter functions
+// Hunter functions
 void initHunter(char *name, HunterType *hunter, RoomType *startingRoom, EvidenceType evType, EvidenceListType *evList, HouseType *house);
 void initHunterList(HunterListType *list);
-void addHunter(HunterListType *list, HunterType *hunter);//adds to the house's master list of hunters
+void addHunter(HunterListType *list, HunterType *hunter); // adds to the house's master list of hunters
 void removeHunter(HunterListType *list, HunterType *hunter);
 void cleanupHunterList(HunterListType *list);
 void* hunterAction(void *arg);
-void hunterMove(HunterType *hunter);//move to random connected room
-void hunterCollect(HunterType *hunter);//collect evidence in the room
-char hunterReview(HunterType *hunter);//review evidence
-char checkGhost(HunterType *hunter);//check if ghost is in the room
+void hunterMove(HunterType *hunter); // move to random connected room
+void hunterCollect(HunterType *hunter); // collect evidence in the room
+char hunterReview(HunterType *hunter); // review evidence
+char checkGhost(HunterType *hunter); // check if ghost is in the room
 
-//Evidence functions
+// Evidence functions
 void initEvidenceList(EvidenceListType *list);
-void addEvidence(EvidenceListType *list, EvidenceType ev);//for hunters to add to the list
-void leaveEvidence(GhostType *ghost);//ghost action to leave evidence behind
+void addEvidence(EvidenceListType *list, EvidenceType ev); // for hunters to add to the list
+void leaveEvidence(GhostType *ghost); // ghost action to leave evidence behind
 void cleanupEvidenceList(EvidenceListType *list);
 
 
@@ -163,9 +160,9 @@ enum GhostClass randomGhost();  // Return a randomly selected a ghost type
 void ghostToString(enum GhostClass, char*); // Convert a ghost type to a string, stored in output paremeter
 void evidenceToString(enum EvidenceType, char*); // Convert an evidence type to a string, stored in output parameter
 
-void hunterNameInput(char* name);//handle entering a name for a hunter
-int evidenceCheck(EvidenceListType *list);//loops through the hunter's evidence list and checks if sufficient or not
-void finalResult(HouseType *house, GhostType *ghost);//displays final results
+void hunterNameInput(char* name);// handle entering a name for a hunter
+int evidenceCheck(EvidenceListType *list);// loops through the hunter's evidence list and checks if sufficient or not
+void finalResult(HouseType *house, GhostType *ghost);// displays final results
 
 // Logging Utilities
 void l_hunterInit(char* name, enum EvidenceType equipment);//used
